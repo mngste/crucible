f0add3c4b1c56e248f0ceafe5e92927b0a01d291
mngt <80961204+mngste@users.noreply.github.com>
2025-06-11 22:22:00 +0200

    Create dotfiles-setup.sh

---
+++ dotfiles-setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+STOW=$(cd config && stow -v -t ~)
+ORIGINAL_DIR=$(pwd)
+REPO_URL="https://github.com/mngste/dotfiles"
+REPO_NAME="dotfiles"
+
+
+is_stow_installed() {
+  dnf list installed "stow" &> /dev/null
+}
+
+if ! is_stow_installed; then
+  echo "Install stow first"
+  exit 1
+fi
+
+cd ~
+
+# Check if the repository already exists
+if [ -d "$REPO_NAME" ]; then
+  echo "Repository '$REPO_NAME' already exists. Skipping clone"
+else
+  git clone "$REPO_URL"
+fi
+
+# Check if the clone was successful
+if [ $? -eq 0 ]; then
+  cd "$REPO_NAME"
+  "$STOW" fish
+  "$STOW" nvim
+  
+else
+  echo "Failed to clone the repository."
+  exit 1
+fi
