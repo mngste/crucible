d491a32cc6d7670dea3cdfc45e61b65c32ab5219
mngt <80961204+mngste@users.noreply.github.com>
2025-06-12 10:58:39 +0200

    Update gnome-hotkeys.sh

--- gnome/gnome-hotkeys.sh
+++ gnome/gnome-hotkeys.sh
@@ -14,12 +14,12 @@ gsettings set org.gnome.shell.keybindings switch-to-application-8 "[]"
 gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"
 
 # Use super for workspaces
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>code10']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>code11']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>code12']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>code13']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>code14']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>code15']"
 
 # Alt+F4 is very cumbersome
 gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
