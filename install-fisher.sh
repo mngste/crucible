da7da0e06a01c90003335893b5202757e053d288
mngt <80961204+mngste@users.noreply.github.com>
2025-06-12 10:23:53 +0200

    Update install-fisher.sh

--- install-fisher.sh
+++ install-fisher.sh
@@ -3,11 +3,22 @@ FISHERS=(
   "joseluisq/gitnow@2.13.0"
 )
 
-for fisher in "${FISHERS[@]}"; do
-  if ! fisher list | grep -i "$fisher" &> /dev/null; then
-    echo "Installing fisher plugin: $fisher"
-    fisher install "$fisher"
-  else
-    echo "Fisher plugin already installed: $fisher"
-  fi
-done
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+if ! fisher list | grep -i "jorgebucaran/fisher" &> /dev/null; then
+    echo "fisher not installed"
+    echo "try to reinstall it"
+    exit 1
+    else 
+      for fisher in "${FISHERS[@]}"; do
+        if ! fisher list | grep -i "$fisher" &> /dev/null; then
+          echo "Installing fisher plugin: $fisher"
+          fisher install "$fisher"
+        else
+          echo "Fisher plugin already installed: $fisher"
+        fi
+      done
+fi
+
+
+
