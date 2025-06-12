06d514cbec804c08bef379d449baef5aee33fa2e
mngt <80961204+mngste@users.noreply.github.com>
2025-06-12 10:10:28 +0200

    Update install-fisher.sh

--- install-fisher.sh
+++ install-fisher.sh
@@ -1,1 +1,13 @@
-#!/usr/bin/bash
+FISHERS=(
+  "PatrickF1/fzf.fish"
+  "joseluisq/gitnow@2.13.0"
+)
+
+for fisher in "${FISHERS[@]}"; do
+  if ! fisher list | grep -i "$fisher" &> /dev/null; then
+    echo "Installing fisher plugin: $fisher"
+    fisher install "$fisher"
+  else
+    echo "Fisher plugin already installed: $fisher"
+  fi
+done
