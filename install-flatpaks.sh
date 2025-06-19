9311a5befc35bdc1c807b8f9ccf7a1b53093f10a
mngste <mangouste@mosaurus.com>
2025-06-19 22:26:24 +0200

    adjust files

--- install-flatpaks.sh
+++ install-flatpaks.sh
@@ -7,7 +7,7 @@ FLATPAKS=(
 for pak in "${FLATPAKS[@]}"; do
   if ! flatpak list | grep -i "$pak" &> /dev/null; then
     echo "Installing Flatpak: $pak"
-    flatpak install --noninteractive "$pak"
+    sudo flatpak install --noninteractive "$pak"
   else
     echo "Flatpak already installed: $pak"
   fi
