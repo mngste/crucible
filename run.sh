b30b388a3bd5c5e6e43cc9017a8e98bb627c551b
mngt <80961204+mngste@users.noreply.github.com>
2025-09-17 20:28:34 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -55,15 +55,15 @@ echo "Installing flatpaks (like discord)"
 . install-flatpaks.sh
 
 # install eza on ubuntu
-echo "Installing eza on ubuntu"
-if [[ "$pm" = "apt" ]]; then
-    sudo mkdir -p /etc/apt/keyrings
-    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-    sudo "$pm" update
-    sudo "$pm" install -y eza
-fi 
+# echo "Installing eza on ubuntu"
+#if [[ "$pm" = "apt" ]]; then
+#    sudo mkdir -p /etc/apt/keyrings
+#    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+#    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+#    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+#   sudo "$pm" update
+#    sudo "$pm" install -y eza
+#fi 
 
 # Install dotfiles
 echo "Installing dotfiles/configurations..."
@@ -72,11 +72,6 @@ echo "Installing dotfiles/configurations..."
 # Making fish default shell
 echo "Running sudo chsh -s /usr/bin/fish && chsh -s /usr/bin/fish to change default shell"
 sudo chsh -s /usr/bin/fish && chsh -s /usr/bin/fish
-
-# install ghostty
-if [[ "$pm" = "apt" ]]; then
-    sudo snap install ghostty --edge --classic
-fi
 
 # disable version check only for switcher and gnome 48 (to remove when working)
 echo "disable version check only for switcher and gnome 48 (to remove when working)"
