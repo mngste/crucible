68f7edf6540e25b664f618402e9e1836e36ef565
mngt <80961204+mngste@users.noreply.github.com>
2025-12-15 13:52:23 +0100

    Update run_srv.sh

--- run_srv.sh
+++ run_srv.sh
@@ -19,24 +19,14 @@ print_logo
 # Exit on any error
 set -e
 
-# ask if ubuntu or fedora
-pm=''
-read -p "On which OS, do you launch the script Fedora, Ubuntu? (f/u/q) " ans;
-
-case $ans in
-    f)pm='dnf';;
-    u)pm='apt';;
-    q)exit;;
-esac
-
 # Update the system first
 echo "Updating system..."
-sudo "$pm" update
+sudo apt update
 
 # Function to install software
 echo "Installing Software"
 if [ -f ./packages_srv.txt ]; then
-  sudo "$pm" install -y $(cat ./packages_srv.txt)
+  sudo apt install -y $(cat ./packages_srv.txt)
   echo "Software has been installed"
 else
   log_action "packages_srv.txt not found"
@@ -49,10 +39,5 @@ echo "Installing dotfiles/configurations..."
 # Making fish default shell
 echo "Running sudo chsh -s /usr/bin/fish && chsh -s /usr/bin/fish to change default shell"
 sudo chsh -s /usr/bin/fish $(whoami)
-
-# Install Rust and Eza
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-. "$HOME/.cargo/env"
-source "$HOME/.cargo/env.fish"
 
 echo "Setup complete! You may want to reboot your system."
