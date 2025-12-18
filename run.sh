5a4408fdc6f8d624d314edb577d6a5464726020d
mngt <80961204+mngste@users.noreply.github.com>
2025-12-18 22:48:09 +0100

    Enhance run.sh with error handling and color output

--- run.sh
+++ run.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/bash
 
+# Exit on any error
+set -e
+
+BLACK="\e[0;30m"
+BLUE="\e[1;34m"
+CYAN="\e[36m"
+RESET="\e[0m"
+UNDERLINE="\e[4m"
+NO_UNDERLINE="\e[24m"
+
+echo -e "${BLACK}"
+
 # Print the logo
 print_logo() {
     cat << "EOF"
     ______                _ __    __     
    / ____/______  _______(_) /_  / /__   
   / /   / ___/ / / / ___/ / __ \/ / _ \  
- / /___/ /  / /_/ / /__/ / /_/ / /  __/  Fedora/Ubuntu Linux System Crafting Tool
+ / /___/ /  / /_/ / /__/ / /_/ / /  __/  fedora/ubuntu/mint Linux System Crafting Tool
  \____/_/   \__,_/\___/_/_.___/_/\___/   by: mngste
 
 EOF
@@ -16,45 +28,59 @@ EOF
 clear
 print_logo
 
-# Exit on any error
-set -e
+echo -e "${BLUE}    Sit back while we install your linux software"
+echo
+
+pm=''
+
+if [[ -f /etc/os-release ]]; then
+    source /etc/os-release
+    if [[ "$ID" === "fedora" ]]; then
+        pm='dnf'
+        exit 1
+    fi
+else
+    if [[ "$ID" === "linuxmint" ]] || [[ "$ID" === "ubuntu" ]]; then
+        pm='apt'
+        exit 1
+    fi
+fi
 
 # Update the system first
-echo "Updating system..."
-sudo apt update
+echo "${CYAN} Updating system..."
+sudo ${pm} update && sudo ${pm} upgrade --refresh
 
 # Function to install software
 echo "Installing Software"
-if [ -f ./packages.txt ]; then
-  sudo apt install -y $(cat ./packages.txt)
+if [[ -f ./packages.txt ]]; then
+  sudo ${pm} install -y $(cat ./packages.txt)
   echo "Software has been installed"
 else
   log_action "packages.txt not found"
 fi
   
 # Install gnome specific things to make it like a tiling WM
-echo "Installing Gnome extensions..."
-. gnome/gnome-extensions.sh
-echo "Setting Gnome hotkeys..."
-. gnome/gnome-hotkeys.sh
-echo "Configuring Gnome..."
-. gnome/gnome-settings.sh
+# echo "Installing Gnome extensions..."
+#. gnome/gnome-extensions.sh
+#echo "Setting Gnome hotkeys..."
+#. gnome/gnome-hotkeys.sh
+#echo "Configuring Gnome..."
+#. gnome/gnome-settings.sh
   
-# Some programs just run better as flatpaks. Like discord/spotify
-echo "Installing flatpaks (like discord)"
+# Some programs just run better as flatpaks. Like vivaldi
+echo "Installing flatpaks"
 . install-flatpaks.sh
 
 # Install dotfiles
-echo "Installing dotfiles/configurations..."
+echo "${UNDERLINE} Installing dotfiles/configurations..."
 . ./dotfiles-setup.sh
 
 # Making fish default shell
 echo "Running sudo chsh -s /usr/bin/fish && chsh -s /usr/bin/fish to change default shell"
 sudo chsh -s /usr/bin/fish $(whoami)
 
-# disable version check only for switcher and gnome 48 (to remove when working)
-#echo "disable version check only for switcher and gnome 48 (to remove when working)"
-#gsettings set org.gnome.shell disable-extension-version-validation "true"
+# install fresh ide terminal
+curl https://raw.githubusercontent.com/sinelaw/fresh/refs/heads/master/scripts/install.sh | sh
 
-echo "Setup complete! You may want to reboot your system."
+echo "${CYAN} Setup complete! You may want to reboot your system."
 
