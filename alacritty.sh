a277c4916c124b1f69ae22e2a1120550d0148754
mngt <80961204+mngste@users.noreply.github.com>
2026-01-06 16:48:26 +0100

    Fix formatting and add missing newline in alacritty.sh

--- alacritty.sh
+++ alacritty.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/bash
 
 git clone https://github.com/alacritty/alacritty.git
 cd alacritty
@@ -18,7 +18,7 @@ cargo build --release
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
 
 ## desktop entry
-sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp target/release/alacritty /usr/local/bin
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
@@ -34,3 +34,5 @@ scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share
 ## autocompletion
 mkdir -p $fish_complete_path[1]
 cp alacritty/extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
+
+cd ~
